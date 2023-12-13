1f62558065ccba33772b32cd7f952f1f3abb85bf
Aicha BIRAT <125543430+Aicha01Isha@users.noreply.github.com>
2023-12-13 21:42:14 +0100

    Delete 0x17-web_stack_debugging_3 directory
    A

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env puppet
-# watrvfrror
-exec { 'web stack deubging':
-  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path     => '/usr/local/bin/:/bin/',
-  provider => 'shell'
-}
